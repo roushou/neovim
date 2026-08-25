c89cf2dcccb9de2a738f8f79f2ec2c91bb2171b1
roushou <roushou9@gmail.com>
2026-08-25 23:41:53 +0900

    refactor(lsp): gate LSP keymaps with supports_method

--- lua/lsp/keys.lua
+++ lua/lsp/keys.lua
@@ -7,26 +7,33 @@ local M = {}
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return -- client stopped between LspAttach and this callback
+		end
 		client.server_capabilities.semanticTokensProvider = nil
 
-		if client.server_capabilities.hoverProvider then
+		if client:supports_method("textDocument/hover", args.buf) then
 			map("n", "K", function()
 				vim.lsp.buf.hover({ border = "single", title = "hover" })
 			end, { buffer = args.buf, desc = "Hover" })
 		end
 
-		map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", { buffer = args.buf, desc = "Rename symbol" })
-		if vim.bo[args.buf].filetype == "rust" then
-			map("n", "<leader>ca", function()
-				vim.cmd.RustLsp("codeAction")
-			end, { buffer = args.buf, desc = "Code action (rust)" })
-		else
-			map(
-				"n",
-				"<leader>ca",
-				"<cmd>lua vim.lsp.buf.code_action()<cr>",
-				{ buffer = args.buf, desc = "Code action" }
-			)
+		if client:supports_method("textDocument/rename", args.buf) then
+			map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", { buffer = args.buf, desc = "Rename symbol" })
+		end
+		if client:supports_method("textDocument/codeAction", args.buf) then
+			if vim.bo[args.buf].filetype == "rust" then
+				map("n", "<leader>ca", function()
+					vim.cmd.RustLsp("codeAction")
+				end, { buffer = args.buf, desc = "Code action (rust)" })
+			else
+				map(
+					"n",
+					"<leader>ca",
+					"<cmd>lua vim.lsp.buf.code_action()<cr>",
+					{ buffer = args.buf, desc = "Code action" }
+				)
+			end
 		end
 	end,
 })
